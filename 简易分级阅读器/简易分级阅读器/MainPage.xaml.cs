83525a3cb30f8d6136d3b2c6a77c7fb571dad6b7
Mr-Jason <inventsoft@126.com>
2014-04-23 20:58:06 +0800

    Windows Phone Application
    Windows Phone Application Training

---
+++ 简易分级阅读器/简易分级阅读器/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace 简易分级阅读器
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // 构造函数
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
